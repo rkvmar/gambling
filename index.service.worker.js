f5b43a1423f4b3260b47df934908f20bbe08916d
Ravi <r2a3v5i7.kumar@gmail.com>
2025-05-29 09:05:57 -0700

    oops i forgot to update

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1748493700|16868773803';
+const CACHE_VERSION = '1748534747|20381833777';
 /** @type {string} */
 const CACHE_PREFIX = 'gambling-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
